fdc885078503014f10771ea7630e8cd5dab5a130
ThePotato97 <10260415+ThePotato97@users.noreply.github.com>
2023-05-23 13:30:40 +0100

    fix package

--- db/connection.js
+++ db/connection.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
-const ENV = process.env.NODE_ENV || 'development';
+const ENV = process.env.NODE_ENV || 'test';
 
 require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-console.log("env", process.env.PGDATABASE)
+
 if (!process.env.PGDATABASE) {
   throw new Error('PGDATABASE not set');
 }
